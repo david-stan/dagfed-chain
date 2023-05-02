7513d1de11b57b9d9b8ba7427977cde7394d8cc4
David Stanojevic <davidstanojevic43@gmail.com>
2023-05-02 14:59:25 +0200

    Fix arbitrary file size transfer.

--- dag_component/dag_socket/client.py
+++ dag_component/dag_socket/client.py
@@ -1,6 +1,9 @@
 import socket
 import sys
 import pathlib
+import struct
+
+BUFFER_SIZE = 1024
 
 def client_trans_require(aim_addr, tx_name):
     try:
@@ -11,25 +14,26 @@ def client_trans_require(aim_addr, tx_name):
         sys.exit(1)
     print(s.recv(1024).decode())
     data = 'requireTx'.encode()
-    s.send(data)
-    response = s.recv(1024)
+    s.send(data) # initiate request
+    response = s.recv(1024) # server agrees
     print("Got response from server", response)
     data = tx_name.encode()
-    s.send(data)
+    s.send(data) # send tx name
     rev_file(s, pathlib.Path('./cache/client/txs') / f"{tx_name}.json")
     print("do ovdje")
     s.close()
 
 def rev_file(conn, tx_file_path):
+    header_size = struct.calcsize('64si')
+    header = conn.recv(header_size)
+    _, tx_size = struct.unpack('64si', header)
+    print(f"Size of the block is {tx_size}")
+    conn.send('header_resp'.encode())
     with open(tx_file_path, 'wb') as f:
-        while 1:
-            buff = conn.recv(1024)
-            print("jjs", buff)
-            if not buff:
-                break
-            print("dosao")
+        bytes_received = 0
+        while not bytes_received >= tx_size:
+            buff = conn.recv(BUFFER_SIZE)
+            bytes_received += len(buff)
             f.write(buff)
-            f.close()
-            break
         
 
--- dag_component/dag_socket/server.py
+++ dag_component/dag_socket/server.py
@@ -1,9 +1,12 @@
 import socket
 import threading
 import sys
+import struct
 import os
 
 from dag import DAG
+
+BUFFER_SIZE = 1024
 
 def create_server_socket(dag_obj, num_shards = 5):
     try:
@@ -22,10 +25,13 @@ def create_server_socket(dag_obj, num_shards = 5):
 
 def file_send(conn: socket, file_addr):
     try:
+        file_header = struct.pack('64si', os.path.basename(file_addr).encode(),
+                                          os.stat(file_addr).st_size)
+        conn.send(file_header) # send header
+        conn.recv(BUFFER_SIZE) # header response
         with open(file_addr, 'rb') as f:
             while True:
-                data = f.read(1024)
-                print(data)
+                data = f.read(BUFFER_SIZE)
                 if not data:
                     break
                 conn.send(data)
